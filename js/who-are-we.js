bdd925756b4025df6cc9aaff4ca32445530b8d64
Dennis Padiernos <depadiernos@gmail.com>
2019-07-24 20:10:31 -0500

    working team member cards

--- js/who-are-we.js
+++ js/who-are-we.js
@@ -1,18 +1,24 @@
 const team = [
-  { name: "Kasit", role: "Web Dev", img: "/img/kasit.jpg" },
-  { name: "Ian", role: "Web Dev", img: "/img/ian.jpg" },
-  { name: "Dennis", role: "Web Dev", img: "/img/dennis.jpg" },
-  { name: "Melvin", role: "Front-end Dev", img: "/img/melvin.jpg" }
+  { name: "Kasit", role: "Web Dev", img: "/img/kasit.png" },
+  { name: "Ian", role: "Web Dev", img: "/img/ian.png" },
+  { name: "Dennis", role: "Web Dev", img: "/img/dennis.png" },
+  { name: "Melvin", role: "Front-end Dev", img: "/img/melvin.png" }
 ]
 
-let aboutUs = document.querySelector("#about-us")
+let aboutUs = document.getElementById("about-us")
 
-function teamMember(member){
-  const div = document.createElement('div')
-  div.innerHTML = `<img src=${member.img}/><h3>${member.name}</h3><h3>${member.role}</h3>`
+function teamMember(member) {
+  const div = document.createElement("div")
+  const memberDetails = `
+  <img src="${member.img}"/>
+  <h3>${member.name}</h3>
+  <h3>${member.role}</h3>
+  `
+  div.innerHTML = memberDetails
+  return div
 }
 
-team.forEach((member)=>{
+team.forEach((member) => {
   let newMember = teamMember(member)
-  aboutUs.append(newMember)
+  aboutUs.appendChild(newMember)
 })
