802fd78b945f9e2a20048de7f6e80c0a50c761d5
Dennis Padiernos <depadiernos@gmail.com>
2019-07-29 20:41:54 -0500

    refactor headerNav to be componentized.

---
+++ js/headernav.js
@@ -0,0 +1,26 @@
+let headerNav = document.getElementById("header-nav")
+
+const nav = document.createElement("nav")
+const navContent = `
+        <div class="logo">
+          <a href="/">
+            <img src="img/zzz-white.png" alt="logo" />
+            <h1>Sleep Tracker</h1>
+          </a>
+        </div>
+        <div class="nav-items">
+          <a href="/what_is_sleep_tracker.html">What is Sleep Tracker?</a>
+          <a href="/why-sleep.html">Why Sleep?</a>
+          <a href="/who-are-we.html">Who are we?</a>
+        </div>
+        <div class="login">
+          <button>
+            <a href="https://app-sleeptracker-july.netlify.com/">Sign Up</a>
+          </button>
+          <button>
+            <a href="https://app-sleeptracker-july.netlify.com/">Log In</a>
+          </button>
+        </div>
+  `
+nav.innerHTML = navContent
+headerNav.appendChild(nav)
