a549f81a67066609087d5213567130fbe181ab49
Dennis Padiernos <depadiernos@gmail.com>
2019-07-31 13:33:20 -0500

    mostly working mobile menu. Need to make js change nav dynamically.

--- js/headernav.js
+++ js/headernav.js
@@ -4,39 +4,45 @@ function navbar() {
   const nav = document.createElement("nav")
   const navContent = `
   <div class="logo">
-  <a href="/">
-  <img src="img/zzz-white.png" alt="logo" />
-  <h1>Sleep Tracker</h1>
-  </a>
-  </div>
-  <div id="menu>
-  <div class="nav-items">
-  <a href="what_is_sleep_tracker.html">What is Sleep Tracker?</a>
-  <a href="why-sleep.html">Why Sleep?</a>
-  <a href="who-are-we.html">Who are we?</a>
-  </div>
-  <div class="login">
-  <button>
-  <a href="https://app-sleeptracker-july.netlify.com/">Sign Up</a>
-  </button>
-  <button>
-  <a href="https://app-sleeptracker-july.netlify.com/">Log In</a>
-  </button>
-  </div>
-  </div>
-  <div id="mobile-menu">
-  <div class="nav-items">
-  <a href="what_is_sleep_tracker.html">What is Sleep Tracker?</a>
-  <a href="why-sleep.html">Why Sleep?</a>
-  <a href="who-are-we.html">Who are we?</a>
-  </div>
-  <div class="login">
-  <button>
-  <a href="https://app-sleeptracker-july.netlify.com/signup">Sign Up</a>
-  </button>
-  <button>
-  <a href="https://app-sleeptracker-july.netlify.com/login">Log In</a>
-  </button>
+    <a href="/">
+        <img src="img/zzz-white.png" alt="logo" />
+        <h1>Sleep Tracker</h1>
+    </a>
+</div>
+<div id="menu">
+    <div class="nav-items">
+        <a href="what_is_sleep_tracker.html">What is Sleep Tracker?</a>
+        <a href="why-sleep.html">Why Sleep?</a>
+        <a href="who-are-we.html">Who are we?</a>
+    </div>
+    <div class="login">
+        <button>
+            <a href="https://app-sleeptracker-july.netlify.com/">Sign Up</a>
+        </button>
+        <button>
+            <a href="https://app-sleeptracker-july.netlify.com/">Log In</a>
+        </button>
+    </div>
+</div>
+<div id="menuToggle">
+  <input type="checkbox" />
+  <span></span>
+  <span></span>
+  <span></span>
+  <div class="mobile-menu">
+    <div class="nav-items">
+        <a href="what_is_sleep_tracker.html">What is Sleep Tracker?</a>
+        <a href="why-sleep.html">Why Sleep?</a>
+        <a href="who-are-we.html">Who are we?</a>
+    </div>
+    <div class="login">
+        <button>
+            <a href="https://app-sleeptracker-july.netlify.com/signup">Sign Up</a>
+        </button>
+        <button>
+            <a href="https://app-sleeptracker-july.netlify.com/login">Log In</a>
+        </button>
+    </div>
   </div>
 </div>
   `
