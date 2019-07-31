8ccbd8e710a36a24a8a0a519b45885acc17cd8ec
Dennis Padiernos <depadiernos@gmail.com>
2019-07-30 21:14:56 -0500

    better approach. less 'blowing up the DOM'. :)

--- js/headernav.js
+++ js/headernav.js
@@ -9,6 +9,7 @@ function navbar() {
   <h1>Sleep Tracker</h1>
   </a>
   </div>
+  <div id="menu>
   <div class="nav-items">
   <a href="what_is_sleep_tracker.html">What is Sleep Tracker?</a>
   <a href="why-sleep.html">Why Sleep?</a>
@@ -22,52 +23,34 @@ function navbar() {
   <a href="https://app-sleeptracker-july.netlify.com/">Log In</a>
   </button>
   </div>
-  `
-  nav.innerHTML = navContent
-  return nav 
-}
-
-function mobileNavbar() {
-  const nav = document.createElement("nav")
-  const navContent = `
-  <div class="logo">
-  <a href="/">
-  <img src="img/zzz-white.png" alt="logo" />
-  <h1>Sleep Tracker</h1>
-  </a>
   </div>
-  <div id="menuToggle">
-
-  <input type="checkbox" />
-  
-  <span></span>
-  <span></span>
-  <span></span>
-  
-  <ul id="menu">
-    <a href="#"><li>Home</li></a>
-    <a href="#"><li>About</li></a>
-    <a href="#"><li>Info</li></a>
-    <a href="#"><li>Contact</li></a>
-    <a href="https://erikterwan.com/" target="_blank"><li>Show me more</li></a>
-  </ul>
+  <div id="mobile-menu">
+  <div class="nav-items">
+  <a href="what_is_sleep_tracker.html">What is Sleep Tracker?</a>
+  <a href="why-sleep.html">Why Sleep?</a>
+  <a href="who-are-we.html">Who are we?</a>
+  </div>
+  <div class="login">
+  <button>
+  <a href="https://app-sleeptracker-july.netlify.com/signup">Sign Up</a>
+  </button>
+  <button>
+  <a href="https://app-sleeptracker-july.netlify.com/login">Log In</a>
+  </button>
+  </div>
 </div>
   `
   nav.innerHTML = navContent
-  return nav 
+  return nav
 }
 
 const checkWidth = () => {
   if (window.innerWidth > 800) {
-    if (headerNav == null){
-      headerNav.appendChild(navbar())
+      //change mobile-menu to display:none and change menu to display:flex
     } else {
-      header
+      //change menu to display:none and change mobile-menu to display:block
     }
-  } else {
-    headerNav.appendChild(mobileNavbar())
-  }
 }
 
-
+headerNav.appendChild(navbar())
 window.addEventListener("resize", checkWidth);
